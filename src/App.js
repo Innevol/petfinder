aff661d7d5a900e08580d63f071fcfa3896be455
Justin Patel <justin.patel@gohealthuc.com>
2018-08-28 14:00:26 -0400

    Checkpoint 1
    Checkpoint 1 - JSX

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from "react";
-import render from "react-dom";
+import { render } from "react-dom";
+import pf from "petfinder-client";
 import Pet from "./Pet";
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
+console.log(process.env.API_KEY, process.env.API_SECRET);
+
 class App extends React.Component {
-    render() {
-        <div>
-            <Pet name="Sunny" animal="Bird" breed="Sun Conure" />
-            <Pet name="Mango" animal="Bird" breed="Sun Conure" />
-            <Pet name="Luna" animal="Dog" breed="Mixed" />
-        </div>
-    }
+  componentDidMount() {
+    const prom = petfinder.breed.list({ animal: "dog" });
+
+    prom.then(console.log, console.error);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        <Pet name="Sunny" animal="Bird" breed="Sun Conure" />
+        <Pet name="Mango" animal="Bird" breed="Sun Conure" />
+        <Pet name="Luna" animal="Dog" breed="Mixed" />
+      </div>
+    );
+  }
 }
 
 render(<App />, document.getElementById("root"));
