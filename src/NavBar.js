8758041424fcf59351f90af872dad40498c264af
Innevol <justinptl@gmail.com>
2018-09-04 00:55:54 -0400

    Checkpoint - React Emotion

--- src/NavBar.js
+++ src/NavBar.js
@@ -15,7 +15,7 @@ to {
 
 const SpyGlass = styled("span")`
   display: inline-block;
-  animation: 1s ${Spin} linear infinite;
+  animation: ${props => props.frequency}s ${Spin} linear infinite;
 `;
 
 const Container = styled("header")`
@@ -31,15 +31,26 @@ const NavLink = styled(Link)`
   }
 `;
 
-const NavBar = () => (
-  <Container>
-    <NavLink to="/">Adopt Me!</NavLink>
-    <NavLink to="/search-params">
-      <SpyGlass aria-label="search" role="img">
-        🔍
-      </SpyGlass>
-    </NavLink>
-  </Container>
-);
+class NavBar extends React.Component {
+  state = { frequency: 10 };
+  halfFrequency = () => this.setState({ frequency: this.state.frequency / 2 });
+  render() {
+    return (
+      <Container>
+        <NavLink to="/">Adopt Me!</NavLink>
+        <NavLink to="/search-params">
+          <SpyGlass
+            onClick={this.halfFrequency}
+            frequency={this.state.frequency}
+            aria-label="search"
+            role="img"
+          >
+            🔍
+          </SpyGlass>
+        </NavLink>
+      </Container>
+    );
+  }
+}
 
 export default NavBar;
